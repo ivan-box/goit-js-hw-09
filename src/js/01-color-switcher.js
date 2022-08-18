0fe00f076af88f4bdd66e60e76656a2c884ed8a8
iboks77 <iboks77@gmail.com>
2022-08-18 11:43:38 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+let btnStart = document.querySelector('[data-start]');
+let btnStop = document.querySelector('[data-stop]');
+let bodyColor = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let idInterval;
+btnStart.addEventListener('click', changeColor);
+btnStop.disabled = true;
+function changeColor(event) {
+  event.target.disabled = true;
+  btnStop.disabled = false;
+  idInterval = setInterval(
+    () => (bodyColor.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+// btnStart.disabled = true;
+
+btnStop.addEventListener('click', stopColor);
+function stopColor(event) {
+  event.target.disabled = true;
+  btnStart.disabled = false;
+  clearTimeout(idInterval);
+}
